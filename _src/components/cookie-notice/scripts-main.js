fa8532c78e0e57f35c66f035cff46e7953283d13
Jerome Toole <jerome@wholegraindigital.com>
2022-09-14 15:10:26 +0200

    Remove excess scripts and switch to plausible.

--- _src/components/cookie-notice/scripts-main.js
+++
@@ -1,9 +0,0 @@
-import CookieNotice from './scripts/CookieNotice.js';
-
-window.addEventListener('DOMContentLoaded', () => {
-    const element = document.querySelector('.cookie-notice');
-
-    if (element) {
-        new CookieNotice(element);
-    }
-});
